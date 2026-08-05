a99b449e4d5ce49eef8747e35a6107fa0ea2ce31
bilalxfna <bilal.faisal036@gmail.com>
2026-08-05 15:36:54 +0500

    backend deployment changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,51 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// async function bootstrap() {
+// 	const app = await NestFactory.create(AppModule)
+
+// 	app.enableCors({
+// 		origin: "http://localhost:6063",
+// 		credentials: true,
+// 	});
+
+// 	app.useGlobalPipes(
+// 		new ValidationPipe({
+// 			whitelist: true,
+// 			transform: true,
+// 		}),
+// 	);
+
+// 	const config = new DocumentBuilder()
+// 		.setTitle('Stock Management API')
+// 		.setDescription('API Documentation')
+// 		.setVersion('1.0')
+// 		.addBearerAuth()
+// 		.build();
+
+// 	const document = SwaggerModule.createDocument(app, config);
+
+// 	SwaggerModule.setup('api', app, document);
+
+// 	const port = process.env.PORT || 6006;
+
+// 	await app.listen(port, "0.0.0.0");
+
+// 	console.log(`Server is listening on ${port}`);
+// }
+
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = Number(process.env.PORT) || 8080;
+  const port = Number(process.env.PORT) || 6006;
 
   await app.listen(port, '0.0.0.0');
 
