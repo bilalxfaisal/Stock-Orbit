b53f836ff7b7441383e6b44dc374ed2cbfaa3161
bilalxfna <bilal.faisal036@gmail.com>
2026-08-05 14:34:37 +0500

    backend deployment changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { WarehouseModule } from './warehouse/warehouse.module';
-import { ContainerModule } from './container/container.module';
-import { CategoryModule } from './category/category.module';
-import { ProductModule } from './product/product.module';
-import { ProductTypeModule } from './product_type/product_type.module';
-import { AuditModule } from './audit/audit.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { InventoryModule } from './inventory/inventory.module';
-import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [WarehouseModule, ContainerModule, CategoryModule, ProductModule, ProductTypeModule, AuditModule, DashboardModule, UserModule, AuthModule, InventoryModule, ConfigModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
