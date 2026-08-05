4bf3203a3a3d1707dd7cc1d528d91da00d69f9e8
bilalxfna <bilal.faisal036@gmail.com>
2026-08-05 14:45:56 +0500

    backend deployment changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,39 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-	app.enableCors({
-		origin: "http://localhost:6063",
-		credentials: true,
-	});
+  const port = Number(process.env.PORT) || 8080;
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			transform: true,
-		}),
-	);
+  await app.listen(port, '0.0.0.0');
 
-	const config = new DocumentBuilder()
-		.setTitle('Stock Management API')
-		.setDescription('API Documentation')
-		.setVersion('1.0')
-		.addBearerAuth()
-		.build();
-
-	const document = SwaggerModule.createDocument(app, config);
-
-	SwaggerModule.setup('api', app, document);
-
-	const port = process.env.PORT || 6006;
-
-	await app.listen(port, "0.0.0.0");
-
-	console.log(`Server is listening on ${port}`);
+  console.log('PORT env =', process.env.PORT);
+  console.log('Address =', app.getHttpServer().address());
 }
 
 bootstrap();
