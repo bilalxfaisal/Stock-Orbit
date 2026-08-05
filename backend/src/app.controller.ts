4284e573c64e354b439503e552b60bf39dddb069
bilalxfna <bilal.faisal036@gmail.com>
2026-08-05 14:21:59 +0500

    backend deployment changes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+
+  
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root() {
+    return "OK";
   }
 }
